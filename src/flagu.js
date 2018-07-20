e5a84d51b34e932cc9e3e38f53b1bd0b8b78680e
Gustavo Paiva <guustavo.paiva@gmail.com>
2018-07-20 18:27:41 -0300

    Métodos de callback para funções de flip e unflip.

--- src/flagu.js
+++ src/flagu.js
@@ -1,10 +1,19 @@
-var $ = require('jquery');
-
 var MemoryGame = require('./../src/memgame.js')
 
 
 function start(size) {
-    var game = new MemoryGame(size);
+    const game = new MemoryGame(size);
+
+    game.flip_callback = id => {
+        document.getElementById('game-card-' + id)
+            .style = "background-image : none; background-color : green;";
+    };
+    game.unflip_callback = id => {
+        document.getElementById('game-card-' + id)
+            .style = "background-image: url(../img/" + game.board[id].value + ".svg), background-color  : none;"
+    };
+
+
     var grid = document.getElementById('game-grid');
     var nav = document.getElementById('nav');
     while (grid.firstChild) {
@@ -13,21 +22,38 @@ function start(size) {
     while (nav.firstChild) {
         nav.removeChild(nav.firstChild);
     }
-    for (var i = 0; i < size*size; i++) {
+    for (var i = 0; i < size * size; i++) {
 
         var game_card = document.createElement('div');
         game_card.className = "game-card";
-        game_card.style= "background-image: url(../img/" + game.board[i].value +".svg), none;"
-        var card  = document.createElement('div');
+        game_card.id = "game-card-" + i;
+        game_card.style = "background-image: url(../img/" + game.board[i].value + ".svg);"
+        game_card.addEventListener('click', function (event) {
+
+            var regex = /game-card-(\d+)/g;
+
+            var card_id = regex.exec(this.id)[1];
+
+            console.log("tentando flippar " + card_id);
+
+            if (game.flip_card(card_id)) {
+                console.log("Flippou!!");
+            } else {
+                console.log("deu ruim!");
+            }
+
+            console.log("ganhou? " + game.won());
+        })
+
+        var card = document.createElement('div');
         if (size == 3) {
             card.className = "five wide column"
             card.id = "game-card-" + i;
-        }
-        else if (size == 4) {
+        } else if (size == 4) {
             card.className = "four wide column"
             card.id = "game-card-" + i;
         }
-        
+
 
         game_card.innerHTML = game.board[i].value
 
--- src/memgame.js
+++ src/memgame.js
@@ -1,41 +1,40 @@
-module.exports = class MemoryGame {
-    constructor(size) {
-        this.size = size;
+module.exports = function MemoryGame(size) {
 
-        this.total_cards = this.size * this.size / 2;
-        this.board = new Array(this.size * this.size);
+    this.size = size;
+    this.total_cards = this.size * this.size / 2;
+    this.board = new Array(this.size * this.size);
 
-        for (var i = 0; i < this.board.length; i++) {
-            this.board[i] = {
-                value: Math.floor(i / 2) + 1,
-                flipped: false
-            };
-        }
-        this.board.sort(() => Math.random() - 0.5);
-        this.board.forEach((card, pos) => card.id = pos);
-        this.points = 0;
+    for (var i = 0; i < this.board.length; i++) {
+        this.board[i] = {
+            value: Math.floor(i / 2) + 1,
+            flipped: false,
+        };
     }
+    this.board.sort(() => Math.random() - 0.5);
+    this.board.forEach((card, pos) => card.id = pos);
+    this.points = 0;
 
-    flip_card(x) {
+
+    this.flip_card = function (x) {
         var card = this.board[x];
         var succeeded = true;
 
         if (this.last_card == null) {
             //There is no card flipped, flip it and save it.
             this.last_card = card;
-            card.flipped = true;
+            this.flip(card);
         } else {
             if (card.flipped == true) {
                 //The same card was flipped, unflip it.
                 succeeded = false;
             } else if (this.last_card.value == card.value) {
                 //The flipped card and the last one has the same value, yay.
-                card.flipped = true;
+                this.flip(card);
                 this.points++;
             } else {
                 //The flipped card and the last one has different values, buu.
-                card.flipped = false;
-                this.last_card.flipped = false;
+                this.unflip(card);
+                this.unflip(this.last_card);
                 succeeded = false;
             }
             this.last_card = null;
@@ -43,7 +42,25 @@ module.exports = class MemoryGame {
         return succeeded;
     }
 
-    won() {
+    this.flip = function (card) {
+        card.flipped = true;
+        this.flip_callback(card.id);
+    }
+
+    this.unflip = function (card) {
+        card.flipped = false;
+        this.unflip_callback(card.id);
+    }
+
+    this.flip_callback = function (id) {
+        console.log("flipped " + id);
+    }
+
+    this.unflip_callback = function (id) {
+        console.log("unflipped " + id);
+    }
+
+    this.won = function () {
         return this.total_cards == this.points;
     }
 }
